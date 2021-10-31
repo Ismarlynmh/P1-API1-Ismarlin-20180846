af19960ba1d7d813602b189e28111493e8e5f7a3
DT <DT@DESKTOP-NNDF78P>
2021-10-31 19:24:38 -0400

    Se creo contexto

--- Entidades/Aporte.cs
+++ Entidades/Aporte.cs
@@ -7,13 +7,22 @@ using System.Threading.Tasks;
 
 namespace P1_API1_Ismarlin_20180846.Entidades
 {
-   public class Aporte
+    class Aporte
     {
         [Key]
-        public int AporteId { get; set; }
-        public DateTime Fecha { get; set; } = DateTime.Now;
-        public string Persona { get; set; }
-        public string Concepto { get; set; }
-        public int Monto { get; set; }
+        public int AporteIid { get; set; }
+        public DateTime Fecha { get; set; }
+        public String Persona { get; set; }
+        public String Concepto { get; set; }
+        public double Monto { get; set; }
+
+        public Aporte()
+        {
+            AporteIid = 0;
+            Fecha = DateTime.Now;
+            Persona = string.Empty;
+            Concepto = string.Empty;
+            Monto = 0;
+        }
     }
 }
