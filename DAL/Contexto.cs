31398e0340164fb36591a874caa6f050bd9e0ac7
DT <DT@DESKTOP-NNDF78P>
2021-10-31 18:36:19 -0400

    Se creo el contexto importando Microsoft EntityFrameworkCore y carpeta de EntidadesSe creo la entidad con su propiedad

---
+++ DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using P1_API1_Ismarlin_20180846.Entidades;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_API1_Ismarlin_20180846.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Aporte> Aportes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = DATA\APORTEControl.db");
+        }
+    }
+}
