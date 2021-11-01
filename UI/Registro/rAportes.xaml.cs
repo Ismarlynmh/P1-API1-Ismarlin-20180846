a686bb7b9c9faa323da71a6ba919565bf4f7f366
DT <DT@DESKTOP-NNDF78P>
2021-10-31 23:30:28 -0400

    Se creo el registro

---
+++ UI/Registro/rAportes.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using P1_API1_Ismarlin_20180846.BLL;
+using P1_API1_Ismarlin_20180846.Entidades;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace P1_API1_Ismarlin_20180846.UI
+{
+    /// <summary>
+    /// Lógica de interacción para rAportes.xaml
+    /// </summary>
+    public partial class rAportes : Window
+    {
+        private Aportes Aporte;
+
+        public rAportes()
+        {
+            InitializeComponent();
+            Aporte = new Aportes();
+            this.DataContext = this.Aporte;
+        }
+        private bool Validar()
+        {
+            bool esValido = true;
+
+            if (PersonaTextBox.Text.Length == 0)
+            {
+                esValido = false;
+                MessageBox.Show("Transaccion Fallida!", "Fallo", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+            if (ConceptoTextBox.Text.Length == 0)
+            {
+                esValido = false;
+                MessageBox.Show("Transaccion Fallida!", "Fallo", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+            if (MontoTextBox.Text.Length == 0)
+            {
+                esValido = false;
+                MessageBox.Show("Transaccion Fallida!", "Fallo", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+            return esValido;
+        }
+        private void Limpiar()
+        {
+
+            this.Aporte = new Aportes();
+            DataContext = this.Aporte;
+        }
+
+        private void BuscarIDButton_Click(object sender, RoutedEventArgs e)
+        {
+            int.TryParse(AportesIDTextBox.Text, out int aporteID);
+            var Aporte = AportesBLL.Buscar(aporteID);
+
+            if (Aporte != null)
+                this.Aporte = Aporte;
+            else
+            {
+                this.Aporte = new Aportes();
+                MessageBox.Show("Este aporte no existe.", "Informacion", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+
+            this.DataContext = this.Aporte;
+        }
+
+        private void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Validar())
+                return;
+
+            var paso = AportesBLL.Guardar(this.Aporte);
+
+            if (paso)
+            {
+                Limpiar();
+                MessageBox.Show("Transaccion Exitosa!", " Exito", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+                MessageBox.Show("Transaccion Fallida!", " Fallo", MessageBoxButton.OK, MessageBoxImage.Error);
+
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+            Aportes existe = AportesBLL.Buscar(this.Aporte.AporteId);
+
+            if (AportesBLL.Eliminar(this.Aporte.AporteId))
+            {
+                Limpiar();
+                MessageBox.Show("Eliminado!", " Exito", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            }
+            else
+                MessageBox.Show("No fue posible eliminarlo!", " Fallo", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
