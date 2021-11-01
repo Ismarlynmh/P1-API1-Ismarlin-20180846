2d331a84ded4d775414b6d83e08a45afc1b9e806
DT <DT@DESKTOP-NNDF78P>
2021-10-31 20:36:29 -0400

    Se creo la migracion, Update-Database con la propiedad Copy always

---
+++ Migrations/20211101002158_Migracion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P1_API1_Ismarlin_20180846.Migrations
+{
+    public partial class Migracion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aportes",
+                columns: table => new
+                {
+                    AporteId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Persona = table.Column<string>(type: "TEXT", nullable: true),
+                    Concepto = table.Column<string>(type: "TEXT", nullable: true),
+                    Monto = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aportes", x => x.AporteId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aportes");
+        }
+    }
+}
